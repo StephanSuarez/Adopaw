f9fd416e088d0d75942e81962878ed83e5ec8d0e
stephan <stephan102002@gmail.com>
2024-10-01 00:36:58 -0500

    feature/responsive home page

---
+++ Adopaw/js/hamb_menu.js
@@ -0,0 +1,7 @@
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const menu = document.querySelector('.menu-bar');
+
+hamburgerBtn.addEventListener('click', () => {
+  menu.classList.toggle('active-menu');
+});
+    
